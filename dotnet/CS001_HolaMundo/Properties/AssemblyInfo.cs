92f5cd7a33891d9eff76b10f9b855f90c7691e7b
sandoval <jszapata72@gmail.com>
2016-08-22 12:40:19 -0500

    Cambiando mi codigo "dotnet" a github, subiendo primer proyecto.

---
+++ dotnet/CS001_HolaMundo/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CS001_HolaMundo")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0e986695-6c8d-43f5-93e5-22ff6b12b47c")]
